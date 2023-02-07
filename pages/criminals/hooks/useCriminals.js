581a166b2b26068df1ac1124129343b1ded9b344
Dina <dinagiacobbi@gmail.com>
2023-02-07 14:35:44 -0900

    Adding yamal

--- pages/criminals/hooks/useCriminals.js
+++ pages/criminals/hooks/useCriminals.js
@@ -4,21 +4,31 @@ export const useCriminals = () => {
   const [criminals, setCriminals] = useState([])
   const [offices, setOffices] = useState(new Set())
 
-  //get a list of field offices from the data
+  const upperCase = (str) => {
+    str[0].toUpperCase() + str.slice(1)
+  }
 
+  //get a list of field offices from the data
+  const canonicalOffices = {
+    lasvegas: 'Las Vegas',
+    neworleans: 'New Orleans',
+    newyork: 'New York',
+  }
   useEffect(() => {
     fetch(
       'https://api.fbi.gov/wanted/v1/list',
-      { cache: "force-cache" }
+      { cache: 'force-cache' }
       // 'https://api.fbi.gov/@wanted?pageSize=10&page=1&sort_on=modified&sort_order=desc'
     )
       .then((res) => res.json())
       .then((json) => {
         setCriminals(json?.items)
         json.items.forEach((item) => {
-          console.log(criminals);
+          console.log(criminals)
           item?.field_offices?.forEach?.((office) => {
-            offices.add(office)
+            // check if office needs to be updated
+
+            offices.add(canonicalOffices[office] || upperCase(office))
           })
         })
       })
